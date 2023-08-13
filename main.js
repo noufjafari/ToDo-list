a5325bfffdf2a8bcd3fc042d45fa60522e2b4c39
noufjafari <140402511+noufjafari@users.noreply.github.com>
2023-08-13 09:26:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let list = []
+let inputbox = document.getElementById("inputbox")
+let listcontainer = document.getElementById("listcontainer")
+
+document.getElementById("btn").addEventListener("click",function(){
+list.push(inputbox.value)
+localStorage.setItem("inputbox", inputbox.value)
+inputbox.value=""
+showList();
+
+})
+
+function showList(){
+    listcontainer.innerHTML=""
+    list.forEach(function(x){
+        listcontainer.innerHTML += "<li><img src=uncheked.svg width=20px>"+x+"</li>"
+
+    })
+
+}
+
